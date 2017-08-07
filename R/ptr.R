ee540c43bcd482474c532469533c3a77c12f19cc
Youngser Park <youngser@gmail.com>
2017-08-07 17:32:22 -0400

    revise ptr

--- R/ptr.R
+++ R/ptr.R
@@ -2,16 +2,19 @@ ptr <- function(g)
 {
     suppressMessages(library(igraph))
 
-    if (class(g) == "igraph") {
-        m <- as.matrix(g[])
-    } else if (class(g) == "Matrix") {
-        m <- as.matrix(g)
-    } else m <- g
+#    if (class(g) == "igraph") {
+#        m <- as.matrix(g[])
+#    } else if (class(g) == "Matrix") {
+#        m <- as.matrix(g)
+#    } else m <- g
 
-    tmp <- m[m != 0]
-    nnz <- length(tmp)
-    rk <- rank(tmp)
-    m[m != 0] <- rk * 2 / (nnz+1)
-    g <- graph.adjacency(m,weighted=TRUE)
+#    tmp <- m[m != 0]
+#    nnz <- length(tmp)
+
+    W <- E(g)$weight
+    nnz <- length(W)
+    rk <- rank(W)
+    E(g)$weight <- rk * 2 / (nnz+1)
+#    g <- graph.adjacency(m,weighted=TRUE)
     return(g)
 }
