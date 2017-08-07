1bd38433f733d6020896668d6499f7a203bcb5bb
Youngser Park <youngser@gmail.com>
2017-08-07 12:13:01 -0400

    add README

--- demo/toygraph.R
+++ demo/toygraph.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages({
 
 print("1. Generating a directed, weighted graph...")
 
+set.seed(123)
 pm <- cbind( c(.2, .001), c(.001, .3) )
 g <- sample_sbm(100, pref.matrix=pm, block.sizes=c(30,70), directed=TRUE)
 summary(g)
