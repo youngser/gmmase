15d9f2f36e8fc4062a8ca25ed33e784b77dd4da7
Youngser Park <youngser@gmail.com>
2017-08-08 11:53:25 -0400

    revise userdata demo

--- demo/usergraph.R
+++ demo/usergraph.R
@@ -5,10 +5,11 @@ suppressPackageStartupMessages({
 })
 
 ## Please set this TRUE to use user's own graph!
-user <- FALSE
+user <- TRUE
 if (user) {
-    fname <- readline(prompt="Enter a file name (e.g., /path/edgelist.txt): ")
-    g <- read_graph(fname, format="edgelist") # please read igraph manual page for details, e.g., for other types of graph format it can handle.
+#    fname <- readline(prompt="Enter a file name (e.g., /path/edgelist.txt): ")
+    fname <- "~/Dropbox/gmmase/data/g-10000.el"
+    g <- read_graph(fname, format="edgelist")
 } else {
     data(g10000)
 }
