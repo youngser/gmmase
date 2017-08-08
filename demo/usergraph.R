21d3560d09a959d47905d54340781f84423e7ddd
Youngser Park <youngser@gmail.com>
2017-08-08 10:04:31 -0400

    revise userdata demo

--- demo/usergraph.R
+++ demo/usergraph.R
@@ -4,11 +4,15 @@ suppressPackageStartupMessages({
     library(gmmase)
 })
 
-## Please uncomment these two lines and comment the line "data(g10000)") to read use graph
-#fname <- readline(prompt="Enter a file name (e.g., /path/edgelist.txt): ")
-#g <- read_graph(fname, format="edgelist") # please read igraph manual page for details, e.g., for other types of graph format it can handle.
-data(g10000)
+## Please set this TRUE to use user's own graph!
+user <- FALSE
+if (user) {
+    fname <- readline(prompt="Enter a file name (e.g., /path/edgelist.txt): ")
+    g <- read_graph(fname, format="edgelist") # please read igraph manual page for details, e.g., for other types of graph format it can handle.
+} else {
+    data(g10000)
+}
 summary(g)
 
-res <- gmmase(g, dmax=2)
+res <- gmmase(g)
 Y <- res$class
