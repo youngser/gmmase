a1922cbe561c657541fc6d0fa7ea23ab369fdc31
Youngser Park <youngser@gmail.com>
2017-08-09 07:49:56 -0400

    add manual pages

--- R/ptr.R
+++ R/ptr.R
@@ -1,24 +1,24 @@
+#'
+#' Run pass-to-rank on a weighted graph.
+#'
+#' It extract edge weight \eqn{W} and divide \eqn{2*R / (|E|+1)} where \eqn{R} is the rank of \eqn{W} and \eqn{|E|} is the number of edges.
+#'
+#' @param g a graph in \code{igraph} format
+#'
+#' @author Youngser Park <youngser@jhu.edu>
+#' @export
+#'
+
 ptr <- function(g)
 {
     suppressMessages(library(igraph))
-
-#    if (class(g) == "igraph") {
-#        m <- as.matrix(g[])
-#    } else if (class(g) == "Matrix") {
-#        m <- as.matrix(g)
-#    } else m <- g
-
-#    tmp <- m[m != 0]
-#    nnz <- length(tmp)
 
     if (is.weighted(g)) {
         W <- E(g)$weight
     } else {
         W <- rep(1,ecount(g))
     }
-    nnz <- length(W)
-    rk <- rank(W)
-    E(g)$weight <- rk * 2 / (nnz+1)
-#    g <- graph.adjacency(m,weighted=TRUE)
+
+    E(g)$weight <- rank(W)*2 / (ecount(g)+1)
     return(g)
 }
